51fdc561c8a61f0847967209fe3e90f45bb4be76
sheikafzal43 <sheikafzal43@gmail.com>
2025-07-20 16:07:47 +0530

    .env done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
-// backend/models/db.js
 import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'lifestream',
-  password: '1234',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-export default pool; // ✅ fixed
+export default pool;
